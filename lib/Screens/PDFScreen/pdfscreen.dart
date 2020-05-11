e06daba5097a56128fe190c8decb96f5646a9fd5
MeitanteiAshour <50237142+MeitanteiAshour@users.noreply.github.com>
2020-05-11 04:15:29 +0200

    Changed PDF library

--- lib/Screens/PDFScreen/pdfscreen.dart
+++ lib/Screens/PDFScreen/pdfscreen.dart
@@ -2,12 +2,10 @@ import 'package:copia/Hive/database.dart';
 import 'package:copia/Provider/pdfscreen_bloc.dart';
 import 'package:copia/Provider/prov_db.dart';
 import 'package:fab_circular_menu/fab_circular_menu.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_pdfview/flutter_pdfview.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:native_pdf_view/native_pdf_view.dart';
 import 'package:provider/provider.dart';
 
 class PDFScreen extends StatefulWidget {
@@ -20,12 +18,25 @@ class PDFScreen extends StatefulWidget {
 
 class _PDFScreenState extends State<PDFScreen> {
   int index;
+  PdfController path;
+
   @override
   void initState() {
+    path = PdfController(
+      document: PdfDocument.openFile(widget.snapshot.pdfAsset),
+    );
+
     setState(() {
       index = widget.index;
     });
+
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    print('here');
+    super.dispose();
   }
 
   @override
@@ -38,39 +49,15 @@ class _PDFScreenState extends State<PDFScreen> {
   Widget build(BuildContext context) {
     final _dbProvider = Provider.of<ProviderDB>(context);
     final _pdfProvider = Provider.of<PDFScreenBloc>(context);
-
+    print(path);
     return Scaffold(
       floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
       floatingActionButton: _circularFab(_dbProvider, _pdfProvider, index),
-      body: Stack(
-        children: <Widget>[
-          GestureDetector(
-            child: PDFView(
-              filePath: widget.snapshot.pdfAsset,
-              enableSwipe: true,
-              gestureRecognizers: {
-                Factory(() => LongPressGestureRecognizer(
-                      duration: Duration(milliseconds: 100),
-                    ))
-              },
-              fitPolicy: FitPolicy.BOTH,
-              onPageError: (s, d) => print(s),
-              defaultPage: 0,
-              onPageChanged: (int current, int total) =>
-                  _pdfProvider.currentPage = current,
-              onError: (error) => _pdfProvider.error = error.toString(),
-              pageFling: true,
-              fitEachPage: true,
-              autoSpacing: true,
-              onRender: (_) => _pdfProvider.ready = true,
-            ),
-          ),
-          _pdfProvider.getError.isEmpty
-              ? !_pdfProvider.isReady
-                  ? Center(child: CircularProgressIndicator())
-                  : Container()
-              : Center(child: Text(_pdfProvider.getError)),
-        ],
+      body: GestureDetector(
+        child: PdfView(
+          controller: path,
+          onPageChanged: (int currPage) => _pdfProvider.currentPage = currPage,
+        ),
       ),
     );
   }
@@ -86,11 +73,15 @@ class _PDFScreenState extends State<PDFScreen> {
             return IconButton(
               icon: Icon(Icons.favorite),
               color: _colorDetector(_pdf, _pdfBloc),
-              onPressed: () => _bloc.addBookmark(
-                currPage: _pdfBloc.currpage,
-                index: widget.index,
-                snapshot: widget.snapshot,
-              ),
+              onPressed: () {
+                setState(() {
+                  _bloc.addBookmark(
+                    currPage: _pdfBloc.currpage,
+                    index: index,
+                    snapshot: widget.snapshot,
+                  );
+                });
+              },
             );
           },
         ),
@@ -104,14 +95,13 @@ class _PDFScreenState extends State<PDFScreen> {
         ? _pdf.bookmarked.contains(_pdfBloc.currpage)
             ? Colors.yellow
             : Colors.white
-        : Colors.white;
+        : Colors.black;
   }
 
   void _bookmarksSheet() {
     showModalBottomSheet(
       context: context,
       builder: (_) => Container(
-        width: 100,
         height: 400,
         child: ValueListenableBuilder(
           valueListenable: Hive.box('name').listenable(),
