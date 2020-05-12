8d9cdd50337d51ec9a675df6e6ecd8874e3f3905
MeitanteiAshour <50237142+MeitanteiAshour@users.noreply.github.com>
2020-05-12 21:17:21 +0200

    Implemented Doc file of pdf

--- lib/Screens/PDFScreen/pdfscreen.dart
+++ lib/Screens/PDFScreen/pdfscreen.dart
@@ -7,6 +7,7 @@ import 'package:fab_circular_menu/fab_circular_menu.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_filereader/flutter_filereader.dart';
 import 'package:flutter_sound/flutter_sound_player.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:hive/hive.dart';
@@ -34,6 +35,7 @@ class _PDFScreenState extends State<PDFScreen> {
   bool hideFab = false;
   Axis direction = Axis.horizontal;
   ScrollController _controller;
+  FlutterTts flutterTts;
   @override
   void initState() {
     initPage();
@@ -63,11 +65,11 @@ class _PDFScreenState extends State<PDFScreen> {
       soundPath: _lastPdf.soundPath,
       totalHours: _lastPdf.totalHours,
     );
-    Hive.box('name')
-        .putAt(index, _pdf)
-        .then((value) => _pdfController.dispose());
+    Hive.box('name').putAt(index, _pdf).then(
+          (value) => _pdfController.dispose(),
+        );
     flutterSoundPlayer.release();
-
+    flutterTts.stop();
     super.dispose();
   }
 
@@ -249,7 +251,7 @@ class _PDFScreenState extends State<PDFScreen> {
           CircleAvatar(
             child: IconButton(
               icon: Icon(Icons.group_work),
-              onPressed: () => {},
+              onPressed: () => pdfDoc(),
             ),
           ),
         ],
@@ -267,6 +269,69 @@ class _PDFScreenState extends State<PDFScreen> {
     } else {
       return Colors.white;
     }
+  }
+
+  void pdfDoc() {
+    showModalBottomSheet(
+      context: context,
+      useRootNavigator: true,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (_, StateSetter setState) {
+            return ValueListenableBuilder(
+              valueListenable: Hive.box('name').listenable(),
+              builder: (_, Box box, child) {
+                PDFDB _pdf = box.getAt(index);
+                if (box.getAt(index).documentPath == null) {
+                  return Column(
+                    children: <Widget>[
+                      Container(
+                        child: Text("You don't have an audio for this PDF"),
+                      ),
+                      Container(
+                        child: IconButton(
+                          icon: Icon(Icons.update),
+                          onPressed: () async {
+                            File _file = await FilePicker.getFile(
+                                type: FileType.custom,
+                                allowedExtensions: ['doc']);
+                            final _modifiedPDF = PDFDB(
+                              bookmarked: _pdf.bookmarked,
+                              insertedDate: _pdf.insertedDate,
+                              lastSeenDate: _pdf.lastSeenDate,
+                              lastVisitedPage: _pdf.lastVisitedPage,
+                              pageNote: _pdf.pageNote,
+                              pdfAsset: _pdf.pdfAsset,
+                              pdfName: _pdf.pdfName,
+                              soundPath: _pdf.soundPath,
+                              thumb: _pdf.thumb,
+                              totalHours: _pdf.totalHours,
+                              documentPath: _file.path ?? null,
+                            );
+                            box.putAt(index, _modifiedPDF);
+                          },
+                        ),
+                      ),
+                    ],
+                  );
+                } else {
+                  return Row(
+                    children: <Widget>[
+                      FileReaderView(
+                        filePath: _pdf.documentPath,
+                        openSuccess: (v) {
+                          print(v);
+                        },
+                      ),
+                    ],
+                  );
+                }
+              },
+            );
+          },
+        );
+      },
+    );
   }
 
   void pdfAudio() {
@@ -394,9 +459,17 @@ class _PDFScreenState extends State<PDFScreen> {
   }
 
   Future _speak() async {
-    FlutterTts flutterTts = FlutterTts();
+    flutterTts = FlutterTts();
+    print(await flutterTts.getVoices);
+    print(await flutterTts.getLanguages);
 
-    flutterTts..setVoice("en-us-x-sfg#male_2-local");
-    await flutterTts.speak("What");
+    flutterTts
+      ..setVoice("en-GB-language")
+      ..setLanguage('ja-JP')
+      ..setPitch(1)
+      ..setSpeechRate(1);
+
+    await flutterTts.speak(
+        "新型コロナウイルス対策で、与野党の一部議員が提案する国会審議のオンライン化の検討が進まない。欧州の議会では、一部でウェブ会議や電子投票の導入が実現。国内でも、企業など社会の様々な場でオンライン化が進むのに、なぜ国会の腰は重いのか");
   }
 }
