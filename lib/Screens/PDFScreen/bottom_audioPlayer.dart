771a039951529f1c18ab19ba8097635835cff4a3
MeitanteiAshour <50237142+MeitanteiAshour@users.noreply.github.com>
2020-05-14 21:23:35 +0200

    Finished Audio module

--- lib/Screens/PDFScreen/bottom_audioPlayer.dart
+++ lib/Screens/PDFScreen/bottom_audioPlayer.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound_player.dart';
 import 'package:hive/hive.dart';
+import 'package:intl/intl.dart' show DateFormat;
+import 'package:intl/date_symbol_data_local.dart';
 
 class BottomAudioPlayer extends StatefulWidget {
   final int index;
@@ -17,20 +22,24 @@ class _BottomAudioPlayerState extends State<BottomAudioPlayer>
   FlutterSoundPlayer flutterSoundPlayer;
   AnimationController _controller;
   bool _isPlaying = false;
-  Stream<PlayStatus> _playerSubscription;
-  double _currentPosition;
+  StreamSubscription _playerSubscription;
+  double sliderCurrentPosition = 0.0;
+  double maxDuration = 1.0;
+  String _playerTxt = '00:00:00';
+
   @override
   void initState() {
     _initializeSoundPlayer();
+    initializeDateFormatting();
     _controller =
         AnimationController(vsync: this, duration: Duration(milliseconds: 500));
-    _currentPosition = 0.0;
     super.initState();
   }
 
   @override
   void dispose() {
     flutterSoundPlayer.release();
+    cancelPlayerSubscriptions();
     super.dispose();
   }
 
@@ -67,17 +76,28 @@ class _BottomAudioPlayerState extends State<BottomAudioPlayer>
               Container(
                 width: 200,
                 child: Slider(
-                  value: _currentPosition,
+                  value: min(sliderCurrentPosition, maxDuration),
                   inactiveColor: Colors.green,
                   activeColor: Colors.green,
-                  onChanged: (double time) {
-                    setState(() => _currentPosition = time);
-                    seekToPlayer((time * 100).round());
+                  onChanged: (double time) async {
+                    if (flutterSoundPlayer.playerState !=
+                        t_PLAYER_STATE.IS_STOPPED)
+                      await flutterSoundPlayer.seekToPlayer(time.toInt());
                   },
-                  max: 1,
+                  max: maxDuration,
+                  label: '$_playerTxt',
+                  onChangeEnd: (e) {
+                    print(e);
+                  },
+                  divisions: maxDuration == 0.0 ? 1 : maxDuration.toInt(),
                   min: 0,
                 ),
               ),
+              Text(
+                '$_playerTxt',
+                style: TextStyle(color: Colors.white),
+              ),
+              Icon(Icons.library_music, color: Colors.orange)
             ],
           ),
         ),
@@ -89,9 +109,12 @@ class _BottomAudioPlayerState extends State<BottomAudioPlayer>
   Future<void> _initializeSoundPlayer() async =>
       flutterSoundPlayer = await FlutterSoundPlayer().initialize();
 
-  void seekToPlayer(int milliSecs) async {
-    String result = await flutterSoundPlayer.seekToPlayer(milliSecs);
-    print('seekToPlayer: $result');
+  void onPauseResumePlayerPressed() {
+    if (flutterSoundPlayer == null) return null;
+    if (flutterSoundPlayer.isPaused || flutterSoundPlayer.isPlaying) {
+      flutterSoundPlayer.resumePlayer();
+    }
+    return null;
   }
 
   Future<void> _pauseAudio() async {
@@ -103,13 +126,54 @@ class _BottomAudioPlayerState extends State<BottomAudioPlayer>
   Future<void> _playAudio(String _soundPath) async {
     _isPlaying = true;
     _controller.forward();
-    await flutterSoundPlayer.startPlayer(_soundPath);
-    // _playerSubscription = flutterSoundPlayer.onPlayerStateChanged
-    //   ..listen(
-    //     (PlayStatus e) {
-    //       if (e != null)
-    //         setState(() => _currentPosition = e.currentPosition / e.duration);
-    //     },
-    //   );
+
+    if (flutterSoundPlayer.isStopped) {
+      await flutterSoundPlayer.startPlayer(_soundPath);
+    } else {
+      onPauseResumePlayerPressed();
+    }
+    _addListeners();
+  }
+
+  void _addListeners() {
+    cancelPlayerSubscriptions();
+    _playerSubscription = flutterSoundPlayer.onPlayerStateChanged.listen(
+      (PlayStatus e) {
+        setState(
+          () {
+            if (e != null) {
+              maxDuration = e.duration;
+              if (maxDuration <= 0) maxDuration = 0.0;
+
+              sliderCurrentPosition = min(e.currentPosition, maxDuration);
+              if (sliderCurrentPosition < 0.0) {
+                sliderCurrentPosition = 0.0;
+              }
+              DateTime date = new DateTime.fromMillisecondsSinceEpoch(
+                  e.currentPosition.toInt(),
+                  isUtc: true);
+              String txt = DateFormat('mm:ss:SS', 'en_GB').format(date);
+              setState(() => _playerTxt = txt.substring(0, 8));
+              if (e.duration == e.currentPosition) {
+                setState(
+                  () {
+                    sliderCurrentPosition = 0.0;
+                    _isPlaying = false;
+                    _controller.reverse();
+                  },
+                );
+              }
+            }
+          },
+        );
+      },
+    );
+  }
+
+  void cancelPlayerSubscriptions() {
+    if (_playerSubscription != null) {
+      _playerSubscription.cancel();
+      _playerSubscription = null;
+    }
   }
 }
