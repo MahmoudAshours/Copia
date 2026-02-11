edfebc6d39f5f0a7f72ea8acc983a0f8f4b83b93
Mahmoud Ashour <mashour365@gmail.com>
2026-02-12 00:32:08 +0200

    Refactor main function to simplify app initialization and remove unused dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,9 @@
-import 'package:copia/Hive/database.dart';
-import 'package:copia/Screens/ControllerScreen/controller_screen.dart';
-import 'package:copia/providers.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'Screens/IntroScreen/intro_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() async {
+import 'core/app.dart';
+
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  final dir = await getApplicationDocumentsDirectory();
-  Hive.init(dir.path);
-  Hive.registerAdapter(PDFDBAdapter());
-  await Hive.openBox('pdfDB');
-  runApp(
-    Providers(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: await checkIntro() ? IntroScreen() : ControllerScreen(),
-      ),
-    ),
-  );
-}
-
-/// Check if it's the user first time in the app.
-Future<bool> checkIntro() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  return prefs.getBool('intro') ?? true;
+  runApp(const ProviderScope(child: CopiaApp()));
 }
